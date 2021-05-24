328208040b2c20a0a83f7e862453a99430d3037d
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-24 23:38:59 +0100

    app updated with less setup code for individual stratgeies as they now exist in their onw strateg specific file

--- index.js
+++ index.js
@@ -1,62 +1,16 @@
-const http = require('http');
-
 const webSocket = require('ws');
-const ta = require('technicalindicators');
 
 const calculateRSI = require('./strategies/rsi');
 const calculateHeikinAshi = require('./strategies/heikinAshi');
 const calculateBollingerBands = require('./strategies/bollingerBands');
 const calculateMacd = require('./strategies/macd');
 
-const stream = 'wss://stream.binance.com:9443/ws/btcusdt@kline_1m';
-const PERIOD = 14;
-const RSI_OVERSOLD = 30;
-const RSI_OVERBOUGHT = 70;
+const stream = 'wss://stream.binance.com:9443/ws/btcusdt@kline_5m';
 
-const inputRSI = {
-  values: [],
-  period: PERIOD
-};
-
-const inputHeikinAshi = {
-  open: [],
-  high: [],
-  low: [],
-  close: [],
-  timestamp: [],
-  volume: []
-};
-
-const inputBollingerBands = {
-  period: PERIOD,
-  values: [],
-  stdDev: 2
-};
-
-const inputMACD = {
-  values: [],
-  fastPeriod: 12,
-  slowPeriod: 26,
-  signalPeriod: 9,
-  SimpleMAOscillator: false,
-  SimpleMASignal: false
-}
-
-const heikinAshiResults = [];
-
-let inPositionRSI = false;
-let inPositionHeikinAshi = false;
-let inPositionBollinger = false;
-let inPositionMacd = false;
-
-let heikinAshiCandle;
-
-const server = http.createServer();
 const ws = new webSocket(stream);
 
 ws.on('open', () => {
   console.log('opened connection');
-  heikinAshiCandle = new ta.HeikinAshi(inputHeikinAshi);
 });
 
 ws.on('message', (message) => {
@@ -69,15 +23,13 @@ ws.on('message', (message) => {
   const close = candle['c'];
 
   if (isCandleClosed) {
-    calculateRSI(close, inputRSI, RSI_PERIOD, inPositionRSI, RSI_OVERBOUGHT, RSI_OVERSOLD);
-    calculateHeikinAshi(heikinAshiCandle, heikinAshiResults, open, high, low, close, inPositionHeikinAshi);
-    calculateBollingerBands(inputBollingerBands, PERIOD, close, inPositionBollinger);
-    calculateMacd(inputMACD, close, 26, inPositionMacd);
+    calculateRSI(close);
+    calculateHeikinAshi(open, high, low, close);
+    calculateBollingerBands(close);
+    calculateMacd(close, 40);
   }
 });
 
 ws.on('close', () => {
   console.log('closed connection');
 });
-
-server.listen(3000);
