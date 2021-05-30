7f78cb9bed0f5ed6afdcf9a66163ae3d38e578cd
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-30 21:18:11 +0100

    backtest updated to take in new test data

--- backtest.js
+++ backtest.js
@@ -1,8 +1,8 @@
-const btcusdt = require('./historicalData/btc-usdt-4-hour.json');
+const btcusdt = require('./historicalData/btc-usdt-4-hour');
 const heikinAshiRsiStrategy = require('./strategies/heikinAshi-rsi');
 
 btcusdt.forEach(candle => {
-  const { price_open, price_close, price_high, price_low } = candle;
+  const [openTime, open, high, low, close] = candle;
 
-  heikinAshiRsiStrategy(price_open.toString(), price_high.toString(), price_low.toString(), price_close.toString());
+  heikinAshiRsiStrategy(parseFloat(open), parseFloat(high), parseFloat(low), parseFloat(close));
 });
