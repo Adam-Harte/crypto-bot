e311d0e23102811a2dace5911894991b22afa55b
Adam-Harte <adamharte11@hotmail.co.uk>
2021-06-03 11:59:13 +0100

    web socket method created to automatically reconnect on close

--- index.js
+++ index.js
@@ -4,26 +4,31 @@ const heikinAshiRsiStrategy = require('./strategies/heikinAshi-rsi');
 
 const stream = 'wss://testnet.binance.vision/ws/btcusdt@kline_15m';
 
-const ws = new webSocket(stream);
+const connect = (stream, reconnectInterval) => {
+  const ws = new webSocket(stream);
 
-ws.on('open', () => {
-  console.log('opened connection');
-});
+  ws.on('open', () => {
+    console.log('opened connection');
+  });
 
-ws.on('message', (message) => {
-  const data = JSON.parse(message);
-  const candle = data['k'];
-  const isCandleClosed = candle['x'];
-  const open = parseFloat(candle['o']);
-  const high = parseFloat(candle['h']);
-  const low = parseFloat(candle['l']);
-  const close = parseFloat(candle['c']);
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    const candle = data['k'];
+    const isCandleClosed = candle['x'];
+    const open = parseFloat(candle['o']);
+    const high = parseFloat(candle['h']);
+    const low = parseFloat(candle['l']);
+    const close = parseFloat(candle['c']);
 
-  if (isCandleClosed) {
-    heikinAshiRsiStrategy(open, high, low, close);
-  }
-});
+    if (isCandleClosed) {
+      heikinAshiRsiStrategy(open, high, low, close);
+    }
+  });
 
-ws.on('close', () => {
-  console.log('closed connection');
-});
+  ws.on('close', () => {
+    console.log('closed connection');
+    setTimeout(connect, reconnectInterval);
+  });
+}
+
+connect(stream, 60000);
