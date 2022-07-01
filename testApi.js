2703ea8f4bc1e167a2ebf28327adfb37f7dc5d12
Adam-Harte <adamharte11@hotmail.co.uk>
2022-07-02 00:17:42 +0100

    allOcoOrders added to api test

--- testApi.js
+++ testApi.js
@@ -3,17 +3,19 @@ const limitOrder = require('./api/limitOrder');
 const ocoOrder = require('./api/ocoOrder');
 const openOrders = require('./api/openOrders');
 const allOrders = require('./api/allOrders');
+const allOcoOrders = require('./api/queryAllOco');
 const orderById = require('./api/orderById');
 const cancelOrders = require('./api/cancelOrders');
 const exchangeInfo = require('./api/exchangeInfo');
 const candleSticks = require('./api/candleSticks');
 
 // account();
-// limitOrder('BTCUSDT', 'BUY', 0.1, 10000.00);
+// limitOrder('BTCUSDT', 'SELL', 0.01, 56500.00);
 // ocoOrder('BTCUSDT', 'SELL', 0.1, 34000.0 * 1.4, 34000.0 * 0.8, 34000.0 * 0.7);
 // openOrders('BTCUSDT');
-// allOrders('BTCUSDT');
-// orderById('BTCUSDT', 1);
+// allOcoOrders();
+// allOrders('BTCUSDT')
+// orderById('BTCUSDT', 1031208);
 // cancelOrders('BTCUSDT');
 // exchangeInfo('BTCUSDT');
-// candleSticks('BTCUSDT', '15m', 10);
+// candleSticks('BTCUSDT', '1m', 1000, 1577836800000).then(res => console.log(JSON.stringify(res.data)));
